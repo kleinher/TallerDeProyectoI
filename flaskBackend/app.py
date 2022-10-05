b51684ca7b11985c1504d7664699088dafaa55ac
Hernán Kleinubing <hernan.k97@gmail.com>
2022-10-05 20:11:59 -0300

    guardar estado

--- flaskBackend/app.py
+++ flaskBackend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
@@ -48,32 +48,34 @@ class DispositivosSchema(ma.SQLAlchemySchema):
 
 @app.route("/")
 def hello_world():
-    dsp =  Dispositivos.query.first()
-    
+    dsp =  Dispositivos.query.filter(Dispositivos._id == 1).first()
     return render_template("home.html",disp=dsp)
 @app.route("/test")
 def test():
-    dsp = Dispositivos.query.first()
+    dsp = Dispositivos.query.filter(Dispositivos._id == 1).first()
     app.logger.info('dispositivos: %s',dsp)
     disp_schema = DispositivosSchema()
     dump = disp_schema.dump(dsp)
-    app.logger.info('dump: %s',dsp)
     return dump
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
-    dsp =  Dispositivos.query.first()
-    try:
-        luz_1 = request.form['luz_1']
-        if luz_1 == 'on':
-            dsp.luz_1 = True
-        else:
-            dsp.luz_1 = False
-        db.session.commit()
-    except:
-        pass
-    return render_template("home.html",disp=dsp)
+    dispositivos_validos = ['luz_1','luz_2','luz_3','luz_4','sensor_movimiento','sensor_luminosidad','sensor_intensidad',]
+    checkValues(dispositivos_validos,request.form)
+    return redirect(url_for('hello_world'))
 
+def checkValues(dispositivos_validos,form):
+    dato_base =  Dispositivos.query.filter(Dispositivos._id == 1).first()
+    try:
+        for d in dispositivos_validos:
+            if d in form:
+                setattr(dato_base, d,True)
+            else:
+                setattr(dato_base, d,False)
+        db.session.commit()
+    except Exception:
+        pass 
+    
 
 @app.before_first_request
 def create_tables():
